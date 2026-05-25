e30c87a06eba8c536d0a437ee6097afe23b51675
Jose Vicente Vicedo Silla <josevicentevicedosilla@gmail.com>
2026-05-25 17:04:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,21 +16,3 @@ const observer = new IntersectionObserver(
 revealItems.forEach(function (item) {
 	observer.observe(item);
 });
-
-const cards = document.querySelectorAll(".game-card, .showcase-panel, .studio-item");
-
-cards.forEach(function (card) {
-	card.addEventListener("mousemove", function (event) {
-		const rect = card.getBoundingClientRect();
-		const x = event.clientX - rect.left;
-		const y = event.clientY - rect.top;
-		const rx = ((y / rect.height) - 0.5) * -4;
-		const ry = ((x / rect.width) - 0.5) * 4;
-
-		card.style.transform = "perspective(900px) rotateX(" + rx + "deg) rotateY(" + ry + "deg)";
-	});
-
-	card.addEventListener("mouseleave", function () {
-		card.style.transform = "";
-	});
-});
